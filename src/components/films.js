62563730606ba5df0dd6547cf0ac2da355806cda
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-09-25 15:00:58 +0300

    typo fix

--- src/components/films.js
+++ src/components/films.js
@@ -1,44 +1,41 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import CardFilms from './card/CardFilms'
 
-const Films = ({match}) =>{
+const Films = ({ match }) => {
 
-    const[films, setFilms] = useState([])
+    const [films, setFilms] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         loadAllFilms()
-      },[])
-      
-      const loadAllFilms = async () => {
+    }, [])
+
+    const loadAllFilms = async () => {
         const allFetchedFilms = await axios.get("https://swapi.dev/api/films")
-        .then((res) => {
-        //   console.log("allFetchedFilms", res.data.results)
-          setFilms(res.data.results)
-        //   console.log("match",match)
-        })
-        
+            .then((res) => {
+                setFilms(res.data.results)
+            })
+
     }
 
-    return(
+    return (
         <div className="m-3">
             <br />
-        
-        <div className="text-center">
-            <h3 className="mb-3">Films</h3>
-        </div>
 
-        <div className="row ">
-            
-            {films.map((f, i)=>(
-                <div className="col m-2" key={i}>
-                    <CardFilms films={f}/>
-                    <hr />
+            <div className="text-center">
+                <h3 className="mb-3">Films</h3>
+            </div>
 
-                </div>
-            ))}
-        </div> 
-        {/* { JSON.stringify(films, null, 9)} */}
+            <div className="row ">
+
+                {films.map((f, i) => (
+                    <div className="col m-2" key={i}>
+                        <CardFilms films={f} />
+                        <hr />
+
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
--- src/components/people.js
+++ src/components/people.js
@@ -1,43 +1,40 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import CardPeople from './card/card'
 
-const Peoples = ({match}) =>{
+const Peoples = ({ match }) => {
 
-    const[people, setPeople] = useState([])
+    const [people, setPeople] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         loadAllPeoples()
-      },[])
-      
-      const loadAllPeoples = async () => {
+    }, [])
+
+    const loadAllPeoples = async () => {
         const allFetchedPeoples = await axios.get("https://swapi.dev/api/people")
-        .then((res) => {
-        //   console.log("allFetchedPeoples", res.data)
-          setPeople(res.data.results)
-        //   console.log("match",match)
-        })
-        
+            .then((res) => {
+                setPeople(res.data.results)
+            })
+
     }
 
-    return(
+    return (
         <div className="m-3">
             <br />
-        <div className="text-center">
-            <h3 className="mb-3">Peoples</h3>
-        </div>
+            <div className="text-center">
+                <h3 className="mb-3">Peoples</h3>
+            </div>
 
-        <div className="row ">
-            
-            {people.map((p, i)=>(
-                <div className="col m-2" key={i}>
-                    <CardPeople people={p}/>
-                    <hr />
+            <div className="row ">
 
-                </div>
-            ))}
-        </div>
-        {/* {JSON.stringify(people)} */}
+                {people.map((p, i) => (
+                    <div className="col m-2" key={i}>
+                        <CardPeople people={p} />
+                        <hr />
+
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
